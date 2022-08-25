b7f215d3dc7f34f831977945411b963e6e88e30b
ecspecial <106328797+ecspecial@users.noreply.github.com>
2022-08-26 02:11:02 +0300

    Added conditional statements to for() loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
 // Define a variable for pokemons and make it point to an array
 let pokemonList = [
-
     // Create an object for pokemon #1
     {
         name: 'Octillery',
-        height: 9,
+        height: 0.9,
         types: ['water']
     },
 
     // Create an object for pokemon #2
     {
         name: 'Loudred',
-        height: 10,
+        height: 1,
         types: ['normal'],
     },
 
     // Create an object for pokemon #3
     {
         name: 'Foongus',
-        height: 2,
+        height: 0.2,
         types: ['grass', 'poison']
     }   
 ];
 
-// Loop that iterates over each array object and prints pokemon name value
+// Loop that iterates over each array object and prints poemon name value
 for (i = 0; i < pokemonList.length; i++) {
-    console.log(pokemonList[i].name);
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+
+    // Condition to print comment on the big pokemon
+    if (pokemonList[i].height > 1.7) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+        document.write(' - Wow that\'s big');
+    } else {
+        console.log(pokemonList[i].name);
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'); 
+    }
 }
