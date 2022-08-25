9529e04ce96ea1304ee16faf5e9ee541ed21897c
ecspecial <106328797+ecspecial@users.noreply.github.com>
2022-08-26 02:17:21 +0300

    Added IIFE function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-// Define a variable for pokemons and make it point to an array
-let pokemonList = [
+let pokemonRepository = (function () {
+
+    let pokemonList = [
     {name: 'Octillery', height: 0.9, types: ['water']},
     {name: 'Loudred', height: 1, types: ['normal']},
     {name: 'Foongus', height: 0.2, types: ['grass', 'poison']},
     {name: 'Lapras', height: 2.5, types: ['ice', 'water']},
     {name: 'Tentacruel', height: 1.6, types: ['water', 'poison']}
-];
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height > 2) {
         document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
         document.write(' - Wow, that\'s big' + '<br>');
