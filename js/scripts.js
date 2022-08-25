903b2990f1f69968d2e29a01e453f24273a341a7
ecspecial <106328797+ecspecial@users.noreply.github.com>
2022-08-26 02:14:24 +0300

    Updated pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,10 @@
 // Define a variable for pokemons and make it point to an array
 let pokemonList = [
-    // Create an object for pokemon #1
-    {
-        name: 'Octillery',
-        height: 0.9,
-        types: ['water']
-    },
-
-    // Create an object for pokemon #2
-    {
-        name: 'Loudred',
-        height: 1,
-        types: ['normal'],
-    },
-
-    // Create an object for pokemon #3
-    {
-        name: 'Foongus',
-        height: 0.2,
-        types: ['grass', 'poison']
-    }   
+    {name: 'Octillery', height: 0.9, types: ['water']},
+    {name: 'Loudred', height: 1, types: ['normal']},
+    {name: 'Foongus', height: 0.2, types: ['grass', 'poison']},
+    {name: 'Lapras', height: 2.5, types: ['ice', 'water']},
+    {name: 'Tentacruel', height: 1.6, types: ['water', 'poison']}
 ];
 
 // Loop that iterates over each array object and prints poemon name value
@@ -28,9 +13,9 @@ for (i = 0; i < pokemonList.length; i++) {
     // Condition to print comment on the big pokemon
     if (pokemonList[i].height > 1.7) {
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-        document.write(' - Wow that\'s big');
+        document.write(' - Wow that\'s big' + '<p>');
     } else {
         console.log(pokemonList[i].name);
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'); 
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<p>'); 
     }
 }
