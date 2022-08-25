bdb5e74e2d6a931ceb90ca0e59f357299007797e
ecspecial <106328797+ecspecial@users.noreply.github.com>
2022-08-26 02:01:11 +0300

    Assigned pokemon list variable and added pokemon objects in an array

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Assign variable for pokemon list and point it to an array
+let pokemonList = [
+
+    // Create an object for pokemon #1
+    {
+        name: 'Octillery',
+        height: 9,
+        types: ['water']
+    },
+
+    // Create an object for pokemon #2
+    {
+        name: 'Loudred',
+        height: 10,
+        types: ['normal'],
+    },
+
+    // Create an object for pokemon #3
+    {
+        name: Foongus,
+        height: 2,
+        types: ['grass', 'poison']
+    }
+];
